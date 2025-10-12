479d2ccec133769ec36c671a7fcf26c4c938fdeb
Victor <vmestrada88@gmail.com>
2025-10-12 05:53:03 -0400

    Keep calm and commit

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+// smart-ops-backend/jest.config.js
+module.exports = {
+  testEnvironment: 'node',
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/coverage/**',
+    '!jest.config.js'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  }
+};
